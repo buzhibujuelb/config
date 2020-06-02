81a9b11e25f57c2f6b5517a8f21941d1802f5a1f
buzhibujue <1752862657@qq.com>
2020-06-03 01:03:57 +0800

    fix bugs

--- config.sh
+++ config.sh
@@ -8,15 +8,15 @@ config_apt(){
     wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py
     sudo python3 oh-my-tuna.py --global
   fi
+  #sudo apt-get update
+  #sudo apt-get upgrade -y
 }
 
 set_ssh(){
-  if test -e "~/.ssh/authorized_keys"
+  if ! test -e ~/.ssh/authorized_keys
   then
-    echo. SSH 已配置。
-  else
     echo '设置 ssh 密钥中'
-    bash <(curl -Ls git.io/ikey.sh) -g liubei121212 -d
+    bash < $(curl -Ls git.io/ikey.sh) -g liubei121212 -d
   fi
 }
 
@@ -37,41 +37,35 @@ install_zsh(){
     echo "/etc/passwd 文件中仍有 bash 更改中"
     sudo sed -i 's#/bin/bash#/bin/zsh#g' /etc/passwd
   fi
-  if test -d "~/.oh-my-zsh"
+  if ! test -d ~/.oh-my-zsh
   then
-    echo 已经安装 oh-my-zsh
-  else
+    echo Installing oh-my-zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   fi
 
-  if test -d "$ZSH_CUSTOM/themes/spaceship-prompt"
+  if ! test -d "$ZSH_CUSTOM/themes/spaceship-prompt"
   then
-    echo 已经安装 spaceship-prompt
-  else
+    echo Installing spaceship-prompt
     git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+    sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
   fi
 
-  if test -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+  if ! test -d ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
   then
-    echo 已经安装 zsh-autosuggestions
-  else
+    echo Installing zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
   fi
-  ln -sf .zshrc ~/.zshrc
+  sudo ln -sf .zshrc ~/.zshrc
 }
 
 install_vim(){
-  echo 'Installing zsh.'
+  echo 'Installing vim.'
   sudo apt-get install vim -y
-  ln -sf .vimrc ~/.vimrc
+  sudo ln -sf .vimrc ~/.vimrc
 }
 
 main(){
   config_apt
-
-  sudo apt-get update
-  sudo apt-get upgrade -y
 
   set_ssh
 
