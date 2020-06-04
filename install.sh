a9985521fb36932527de8de132d12a960eabd34a
buzhibujue <1752862657@qq.com>
2020-06-04 12:03:02 +0800

    set git config store to global

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ install_git(){
   git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
   git config --global user.email "1752862657@qq.com"
   git config --global user.name "buzhibujue"
-  git config credential.helper store
+  git config --global credential.helper store
 }
 
 config_proxy(){
